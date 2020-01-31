5c915dcbb9f2b32ef823bc226d5b8a9a5e816920
Newname <new@email>
2020-01-31 14:57:34 +0100

    make test pass

--- src/main/java/org/foo/bar/ShoppingCart.java
+++ src/main/java/org/foo/bar/ShoppingCart.java
@@ -1,27 +1,39 @@
 package org.foo.bar;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShoppingCart {
     private final int quantity;
     private final Product product;
 
-    //TODO check for nulls
-    public ShoppingCart(int quantity, Product product) {
+    private final Map<Product, Integer> content;
+
+
+    private ShoppingCart(int quantity, Product product, Map<Product, Integer> content) {
         this.quantity = quantity;
         this.product = product;
+        this.content = content;
     }
 
     public static ShoppingCart empty() {
-        return new ShoppingCart(0, null);
+        return new ShoppingCart(0, null, Collections.emptyMap());
     }
 
+    //TODO check for nulls
     public ShoppingCart add(int quantity, Product product) {
-        return new ShoppingCart(quantity + this.quantity, product);
+        Integer currentQuantity = this.content.getOrDefault(product, 0);
+        Map<Product, Integer> newContent = new HashMap<>(this.content);
+        newContent.put(product, currentQuantity + quantity);
+
+        return new ShoppingCart(quantity + this.quantity, product, newContent);
     }
 
     public boolean contains(int quantity, Product product) {
-        return quantity == this.quantity && this.product.equals(product);
+        Integer integer = this.content.get(product);
+        return quantity == integer;
     }
 
     public BigDecimal getTotalPrice() {
