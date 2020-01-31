47c4f5f53ab7c8ee11814f5873fc06e4524eb55f
Newname <new@email>
2020-01-31 16:29:30 +0100

    refactor test by removing duplication

--- src/test/java/org/foo/bar/AddMultipleDifferentProductsToShoppingCartTest.java
+++ src/test/java/org/foo/bar/AddMultipleDifferentProductsToShoppingCartTest.java
@@ -1,6 +1,7 @@
 package org.foo.bar;
 
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -13,17 +14,33 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @DisplayName("On adding different products to shopping cart")
 public class AddMultipleDifferentProductsToShoppingCartTest {
+    private final int doveSoapQuantity = 2;
+    private final int axeDeoQuantity = 2;
+    private ShoppingCart shoppingCart;
+
+    @BeforeEach
+    void setUpShoppingCartWithMultipleTypesOfProducts() {
+        shoppingCart = ShoppingCart.empty()
+                .add(doveSoapQuantity, DoveSoap)
+                .add(axeDeoQuantity, AxeDeo)
+                .withTaxRate(new BigDecimal("12.5"));
+    }
 
     @Test
     public void shouldContainAllAddedProducts() {
-        ShoppingCart shoppingCart = ShoppingCart.empty().add(2, TestFixtures.DoveSoap).add(2, TestFixtures.AxeDeo);
+        assertQuantityOfProduct(doveSoapQuantity, DoveSoap);
+        assertQuantityOfProduct(axeDeoQuantity, AxeDeo);
+    }
 
-        assertThat("expected " + 2 + " times" + DoveSoap,
-                shoppingCart.contains(2, DoveSoap), Matchers.is(true));
+    private void assertQuantityOfProduct(int quantity, Product product) {
+        assertThat("expected " + quantity + " times " + product,
+                shoppingCart.contains(quantity, product), Matchers.is(true));
+    }
 
-        assertThat("expected " + 2 + " times" + AxeDeo,
-                shoppingCart.contains(2, DoveSoap), Matchers.is(true));
-
+    @Test
+    public void shallCalculateTotalPriceIncludingTax() {
+        assertThat(
+                shoppingCart.getTotalPrice(), Matchers.is(new BigDecimal("314.96")));
     }
 
     @Nested
@@ -59,18 +76,6 @@ public class AddMultipleDifferentProductsToShoppingCartTest {
             assertThat(
                     shoppingCart.getTotalSalesTax(), Matchers.is(DoveSoap.getUnitPrice()));
         }
-    }
-
-    @Test
-    public void shallCalculateTotalPriceIncludingTax() {
-        ShoppingCart shoppingCart = ShoppingCart.empty()
-                .add(2, TestFixtures.DoveSoap)
-                .add(2, TestFixtures.AxeDeo)
-                .withTaxRate(new BigDecimal("12.5"));
-
-
-        assertThat(
-                shoppingCart.getTotalPrice(), Matchers.is(new BigDecimal("314.96")));
     }
 
 
