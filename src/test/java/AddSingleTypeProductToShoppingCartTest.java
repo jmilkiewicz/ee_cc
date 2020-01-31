0a46a2354f0bea66a5c4f711deb973304f01bbcb
Newname <new@email>
2020-01-31 13:37:27 +0100

    test rename

--- src/test/java/AddSingleTypeProductToShoppingCartTest.java
+++ src/test/java/AddSingleTypeProductToShoppingCartTest.java
@@ -2,20 +2,24 @@ import lombok.val;
 import org.foo.bar.Product;
 import org.foo.bar.ShoppingCart;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@DisplayName("On adding single type products")
 public class AddSingleTypeProductToShoppingCartTest {
 
     @Test
-    public void shallReturnEmptyWhenInputIsEmpty() {
+    public void shouldCheckIfProductsWereAdded() {
         Product product = new Product("Dove Soap", new BigDecimal("39.99"));
 
         val shoppingCart = ShoppingCart.empty().add(5, product);
 
         assertThat("expected " + 5 + " times" + product, shoppingCart.contains(5, product), Matchers.is(true));
     }
+
+
 }
